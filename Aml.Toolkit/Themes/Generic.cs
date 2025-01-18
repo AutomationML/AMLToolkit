f3af238220a51f9ddfe9f8780c2ea88c81b03e08
Josef Prinz <DevSpace@berlin-prinz.de>
2025-01-18 15:08:02 +0100

    v6.0.4.11

--- Aml.Toolkit/Themes/Generic.cs
+++ Aml.Toolkit/Themes/Generic.cs
@@ -12,10 +12,11 @@ namespace Aml.Toolkit.Themes
 {
     public partial class  Generic
     {
-        public void OnMouseLeftButtonDown(object sender, RoutedEventArgs e)
-        {
-        }
-
+        /// <summary>
+        /// AMLNode double click event handler. This handler calls <see cref="Operations.AmlNodeHandler.OnNodeDoubleClick"/>
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         public void OnMouseDoubleClick(object sender, RoutedEventArgs e)
         {
             if (sender is TreeViewItem t && t.DataContext is AMLNodeViewModel nodeViewModel)
